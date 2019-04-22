6eff514c8937a0c9767cfcdbba15a700dea9ad85
avaneeshp5-7 <avaneeshp507@gmail.com>
2019-04-23 02:16:00 +0530

    ffd

--- Services/mobileAPI.js
+++ Services/mobileAPI.js
@@ -3,7 +3,7 @@ router = exp.Router()
 mg = require("mongojs")
 con = mg("mongodb://jai:jai15507@ds149885.mlab.com:49885/ecodb")
 router.get("/getmob",function(req,res){
-    con.product.find({sscatid:"30"}).sort({_id:1}).limit(6,function(err,mresult){
+    con.product.find({sscatid:"30"}).sort({_id:1}).limit(8,function(err,mresult){
         res.send(mresult)
        // console.log(mresult.length)
     })
